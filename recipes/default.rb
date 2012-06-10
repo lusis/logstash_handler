d0557cf7b702c5112bd1664624dde0e93996b1a6
John E. Vincent <lusis.org+github.com@gmail.com>
2012-06-10 00:50:01 -0400

    add license to recipe and convert to full compile phase exec

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,31 @@
+#
+# Author:: John E. Vincent (<lusis.org+github.com@gmail.com>)
+# Cookbook Name:: logstash_handler
+# Recipe:: default
+#
+# Copyright 2012, John E. Vincent
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 include_recipe "chef_handler"
 
 
 cookbook_file "#{node['chef_handler']['handler_path']}/logstash_handler.rb" do
   source "logstash_handler.rb"
   mode "0600"
-end
+  action :nothing
+end.run_action(:create)
 
 chef_handler "Chef::Handler::Logstash" do
   source "#{node['chef_handler']['handler_path']}/logstash_handler.rb"
@@ -14,5 +35,5 @@ chef_handler "Chef::Handler::Logstash" do
               :tags => node['chef_client']['handler']['logstash']['tags'],
               :timeout => node['chef_client']['handler']['logstash']['timeout']
             ]
-  action :enable
-end
+  action :nothing
+end.run_action(:enable)
